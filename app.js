f618953457a22a65a3a6e777d9bbe39537d9073e
Piotr Pacewicz <piotrp92@gmail.com>
2021-03-24 20:05:30 +0000

    add arrow to design and fix functionality

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 const buttons = document.querySelectorAll(".faq__question__button");
-
+const arrows = document.querySelectorAll(".faq__question__button")
 
 const changeButton = ({
     target
 }) => {
-    const prevArticle = target.previousElementSibling;
-    const activeArticle = target.nextElementSibling;
+    let prevArticle ;
+    let activeArticle ;
+    if(target.tagName === 'BUTTON'){
+
+         prevArticle = target.previousElementSibling;
+     activeArticle = target.nextElementSibling;
+    } else {
+        prevArticle = target.parentElement.previousElementSibling;
+        activeArticle = target.parentElement.nextElementSibling;
+    }
+   
     const activeButton = target
-    console.log(target.nextElementSibling)
+    
+    
     activeArticle.classList.toggle("faq__hide-question--active");
     if (activeArticle.classList.contains("faq__hide-question--active")) {
-        activeButton.textContent = "-";
+        
+        activeButton.style.transform = 'rotate(180deg)'
         prevArticle.style.fontWeight = 700;
         prevArticle.style.color = 'hsl(238, 29%, 16%)';
     } else {
-        activeButton.textContent = "+";
+        
+        activeButton.style.transform = 'rotate(0deg)'
         prevArticle.style.fontWeight = 'normal'
     }
 }
 
+
 buttons.forEach(button => {
     button.addEventListener('click', changeButton)
+})
+
+arrows.forEach(arrow=> {
+    arrow.addEventListener('click', changeButton)
 })
