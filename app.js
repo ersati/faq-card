472664306fee37684f15af1c98e86c5fb864fa62
Piotr Pacewicz <piotrp92@gmail.com>
2021-03-26 13:57:20 +0000

    add animation to the box and paragraph

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const buttons = document.querySelectorAll(".faq__question__button");
 const arrows = document.querySelectorAll(".faq__question__button")
-
+const questions = document.querySelectorAll(".faq__question");
+const box =document.querySelector('.box')
 const changeButton = ({
     target
 }) => {
     let prevArticle ;
     let activeArticle ;
+    let activeButton;
     if(target.tagName === 'BUTTON'){
 
          prevArticle = target.previousElementSibling;
      activeArticle = target.nextElementSibling;
-    } else {
+     activeButton = target;
+    } else if(target.tagName === 'IMG') {
         prevArticle = target.parentElement.previousElementSibling;
         activeArticle = target.parentElement.nextElementSibling;
+        activeButton = target;
+    } else if(target.tagName === 'P') {
+        prevArticle = target;
+        activeButton = target.nextElementSibling;
+        activeArticle = activeButton.nextElementSibling; 
     }
    
-    const activeButton = target
+    
     
     
     activeArticle.classList.toggle("faq__hide-question--active");
@@ -32,10 +40,29 @@ const changeButton = ({
 }
 
 
+const changeStyle = ({
+    target
+}) => {
+target.style.color ='hsl(14, 88%, 65%)';
+box.style.transform = 'translate(1%, 120%)';
+setTimeout(function() {
+   target.style.color = "";
+   box.style.transform = 'translate(26%, 118%)'
+  }, 500);
+
+}
+
 buttons.forEach(button => {
     button.addEventListener('click', changeButton)
 })
 
 arrows.forEach(arrow=> {
     arrow.addEventListener('click', changeButton)
+})
+
+questions.forEach(arrow=> {
+    arrow.addEventListener('click', changeButton)
+})
+questions.forEach(arrow=> {
+    arrow.addEventListener('mouseover', changeStyle)
 })
